5a7f38abb9896ea660880f0ee625428f10544d63
Ravi <samcollins9899@gmail.com>
2025-05-16 11:26:34 +0530

    feat: added clerk auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,32 @@
 import { Stack } from "expo-router";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { tokenCache } from "@/cache";
+
+const publishablekey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishablekey) {
+  throw new Error(
+    "Missing Publishable key, Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in you env. ",
+  );
+}
+
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false }}>
-          {/* <Stack.Screen name="index" options={{ title: "Feed" }}></Stack.Screen>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishablekey}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+            <Stack screenOptions={{ headerShown: false }}>
+              {/* <Stack.Screen name="index" options={{ title: "Feed" }}></Stack.Screen>
           <Stack.Screen
             name="notifications"
             options={{ title: "Notifications", headerShown: false }}
           /> */}
-        </Stack>
-      </SafeAreaView>
-    </SafeAreaProvider>
+            </Stack>
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
